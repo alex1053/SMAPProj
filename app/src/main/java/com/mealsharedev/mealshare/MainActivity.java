53215cdf5f05883cbb93fc8292311b019a4ab31b
au533036 <frederik.rosenoern.jensen@post.au.dk>
2018-04-30 09:39:18 +0200

    Facebook login and common header

--- app/src/main/java/com/mealsharedev/mealshare/MainActivity.java
+++ app/src/main/java/com/mealsharedev/mealshare/MainActivity.java
@@ -1,13 +1,22 @@
 package com.mealsharedev.mealshare;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends headerActivity {
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        setHeadings(intent.getStringExtra("user"));
     }
 }
