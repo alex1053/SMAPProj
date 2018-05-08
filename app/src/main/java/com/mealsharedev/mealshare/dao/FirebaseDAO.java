4233426a9e33020c28f72004bc66ae7ac40d4cba
Sander140 <alex.bruhn87@gmail.com>
2018-05-08 09:43:07 +0200

    DAO - initial commit

---
+++ app/src/main/java/com/mealsharedev/mealshare/dao/FirebaseDAO.java
@@ -0,0 +1,24 @@
+package com.mealsharedev.mealshare.dao;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.mealsharedev.mealshare.Models.Comment;
+
+/**
+ * Created by alexb on 08-05-2018.
+ */
+
+public class FirebaseDAO {
+    public static void getMyMeals(String userID) {
+
+    }
+
+    public static void getAssignedMeals(String userID) {
+
+    }
+
+    public static void putComment(Comment comment) {
+
+    }
+
+
+}
