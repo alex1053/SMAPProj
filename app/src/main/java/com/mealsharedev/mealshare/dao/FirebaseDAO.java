0824eddfdba3bfa2bbb1b57ff850f6a9eb1d2ff6
Sander140 <alex.bruhn87@gmail.com>
2018-05-08 10:14:36 +0200

    DAO - actions

--- app/src/main/java/com/mealsharedev/mealshare/dao/FirebaseDAO.java
+++ app/src/main/java/com/mealsharedev/mealshare/dao/FirebaseDAO.java
@@ -2,23 +2,38 @@ package com.mealsharedev.mealshare.dao;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.mealsharedev.mealshare.Models.Comment;
-
-/**
- * Created by alexb on 08-05-2018.
- */
+import com.mealsharedev.mealshare.Models.Meal;
 
 public class FirebaseDAO {
+    public static final String DAO_GET_ALL_MEALS = "getAllMeals";
+    public static final String DAO_GET_MY_MEALS = "getMyMeals";
+    public static final String DAO_GET_RESERVED_MEALS = "getReservedMeals";
+    public static final String DAO_GET_COMMENTED_MEALS = "getCommentedMeals";
+    public static final String DAO_GET_COMMENTS = "getCommentsForMeal";
+
     private FirebaseFirestore mFF;
 
     public FirebaseDAO() {
         mFF = FirebaseFirestore.getInstance();
     }
 
+    public void getAllMeals() {
+
+    }
+
     public void getMyMeals(String userID) {
 
     }
 
-    public void getAssignedMeals(String userID) {
+    public void getReservedMeals(String userID) {
+
+    }
+
+    public void getCommentedMealsForUser(String userID) {
+
+    }
+
+    public void getCommentsForMeal(String MealID) {
 
     }
 
@@ -26,5 +41,7 @@ public class FirebaseDAO {
 
     }
 
+    public void putMeal(Meal meal) {
 
+    }
 }
